920a11841b93e5364f3358003b46fc324e17a085
miretto <miro.jutz@students.fhnw.ch>
2023-04-03 11:27:04 +0200

    Update Uebung 3.py
    Aus Ordner Kapitel 3 kopiert

--- Uebung 3.py
+++ Uebung 3.py
@@ -9,13 +9,16 @@ class Figur:
     
     def __str__(self):
         return self.name
-
+    
 class Punkt(Figur):
     def __init__(self, x, y):
         super().__init__("Punkt")
         self.x = x
         self.y = y
-   
+
+    def __str__(self):
+        return f"Punkt ({self.x}, {self.y})"
+  
 class Dreieck(Figur):
     def __init__(self, ax, ay, bx, by, cx, cy):
         super().__init__("Dreieck")
@@ -33,19 +36,15 @@ class Dreieck(Figur):
     def __str__(self):
         return f"Dreieck mit den Seitenlängen {self.a}, {self.b}, {self.c}"
 
-
 class Rechteck(Figur):
-    def __init__(self, ax, ay, bx, by):
+    def __init__(self, punkt1, punkt2):
         super().__init__("Rechteck")
-        self.A = Punkt(ax,ay)
-        self.B = Punkt(bx,by)
-
-        self.x = abs(ax-bx)
-        self.y = abs(ay-by)
+        self.A = list(punkt1)
+        self.B = list(punkt2)
 
     def Umfang (self):
-        return 2*(self.x + self.y)
-    
+        return 2*(abs(self.A[0]-self.B[0]+self.A[1]-self.B[1]))
+
     def __str__(self):
         return f"Rechteck mit den Seitenlängen {self.x}, {self.y}"
 
@@ -63,11 +62,29 @@ class Kreis(Figur):
     def __str__(self):
         return f"Kreis M={self.mittelpunkt} r={self.radius}"
     
+class Polygon(Figur):
+    def __init__(self, *args):
+        super().__init__("Polygon")
+        self.args = args
 
-d = Kreis(4,6,8)
+    def Umfang(self):
+        umfang = 0
+        for i in range(len(self.args)):
+            aktuellerpkt = self.args[i]
+            naechsterpkt = self.args[(i+1) % len(self.points)]
+            umfang += math.sqrt((naechsterpkt.x - aktuellerpkt.x)**2 + (naechsterpkt.y - aktuellerpkt.y)**2)
+        return umfang
 
+
+d =  Kreis(Punkt(2,3), 5, 8)
+
+print(d.Umfang())
 print(d.__str__())
 
-z = Rechteck(4,5,6,7)
+#salami = Polygon(Punkt(3,4), Punkt(5,6))
+#umfang = salami.Umfang()
 
-print(z.__str__()
+#print(umfang)
+
+
+
